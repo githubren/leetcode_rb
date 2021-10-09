ac3b222ea20d00c9c7a683c7e17736302660c00a
renbing <970910609@qq.com>
2021-10-09 14:48:26 +0800

    86. 分隔链表

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -318,11 +318,11 @@ object Main {
 //        println(lengthOfLongestSubstring2("ckilbkd"))
 
         val node1 = ListNode(1)
-        val node2 = ListNode(2)
+        val node2 = ListNode(4)
         val node3 = ListNode(3)
-        val node4 = ListNode(3)
-        val node5 = ListNode(4)
-        val node6 = ListNode(4)
+        val node4 = ListNode(2)
+        val node5 = ListNode(5)
+        val node6 = ListNode(2)
         val node7 = ListNode(5)
         val node8 = ListNode(5)
         node1.next = node2
@@ -330,8 +330,8 @@ object Main {
         node3.next = node4
         node4.next = node5
         node5.next = node6
-        node6.next = node7
-        node7.next = node8
+//        node6.next = node7
+//        node7.next = node8
 
         val nodea = ListNode(9)
         val nodeb = ListNode(9)
@@ -413,7 +413,13 @@ object Main {
 //            result = result.next
 //        }
 
-        var result = deleteDuplicates(node1)
+//        var result = deleteDuplicates(node1)
+//        while (result != null){
+//            print("${result.n} ")
+//            result = result.next
+//        }
+
+        var result = partition(node1,6)
         while (result != null){
             print("${result.n} ")
             result = result.next
@@ -3495,4 +3501,36 @@ object Main {
         return newHead
     }
 
+    /**
+     * 86. 分隔链表
+     *
+     * 1->4->3->2->5->2
+     */
+    fun partition(head: ListNode?, x: Int): ListNode? {
+        val small = ListNode(0)
+        var st = small
+        val noSmall = ListNode(0)
+        var nst = noSmall
+        var node = head
+        var isSmall = false
+        while (node != null){
+            if (node.n?:0 < x){
+                st.next = node
+                st = st.next!!
+                isSmall = true
+            }else{
+                nst.next = node
+                nst = nst.next!!
+                isSmall = false
+            }
+            node = node.next
+        }
+        if (isSmall){
+            nst.next = null
+        }else{
+            st.next = null
+        }
+        st.next = noSmall.next
+        return small.next
+    }
 }
