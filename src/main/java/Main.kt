5a6afd34331a452cd297067fdd4de1ce198d4f13
renbing <970910609@qq.com>
2022-03-18 13:47:27 +0800

    100. 相同的树

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -5960,4 +5960,15 @@ object Main {
         }
     }
 
+    /**
+     * 100. 相同的树
+     */
+    fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
+        if (p == null && q == null) return true
+        if (p != null && q == null) return false
+        if (p == null && q != null) return false
+        if (p?.`val` != q?.`val`) return false
+        return isSameTree(p?.left,q?.left) && isSameTree(p?.right,q?.right)
+    }
+
 }
